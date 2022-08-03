87bed84e7975b45ea995aaa17145149864938c99
Marge <marge.consunji@gmail.com>
2022-08-03 16:21:03 +0800

    Show anecdote with most votes

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -1,58 +1,51 @@
-const Header = ({ courseName }) => {
-  return (
-    <h1>{courseName}</h1>
-  )
-}
-
-const Part = ({ partName, numEx }) => {
-  return (
-    <p>{partName} {numEx}</p>
-  )
-}
-
-const Content = ({ parts }) => {
-  return (
-    <>
-      <Part partName={parts[0].name} numEx={parts[0].exercises} />
-      <Part partName={parts[1].name} numEx={parts[1].exercises} />
-      <Part partName={parts[2].name} numEx={parts[2].exercises} />
-    </>
-  )
-}
-
-const Total = ({ parts }) => {
-  const total = parts.reduce((acc, { exercises }) => acc + exercises, 0);
-  return (
-    <p>Number of exercises {total}</p>
-  )
-}
+import { useState } from 'react';
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.'
+  ];
+   
+  const [selected, setSelected] = useState(0);
+  const [points, setPoints] = useState({ 0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0 });
+
+  const voteAnecdote = (selectedAnecdote) => {
+    setPoints({ ...points, [selectedAnecdote]: points[selectedAnecdote] + 1})
   }
+
+  const getRandomAnecdote = () => {
+    let randomIndex = Math.floor(Math.random() * anecdotes.length);
+    setSelected(randomIndex);
+  }
+
+  const mostVotes = Object.keys(points).reduce((curr, next) => points[curr] > points[next] ? curr : next);
 
   return (
     <div>
-      <Header courseName={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
+      <h1>Anecdote of the day</h1>
+      <div>
+        <blockquote style={{borderLeft: '6px solid black', padding: '1rem'}}>
+          <p>"{anecdotes[selected]}"</p>
+        </blockquote>
+        <p>This anecdote has {points[selected]} votes</p>
+      </div>
+      <button onClick={() => voteAnecdote(selected)}>vote</button>
+      <button onClick={() => getRandomAnecdote()}>show random anecdote</button>
+
+      <h1>Anecdote with most votes</h1>
+      <div>
+        <blockquote style={{borderLeft: '6px solid black', padding: '1rem'}}>
+          <p>"{anecdotes[mostVotes]}"</p>
+        </blockquote>
+        <p>This anecdote has {points[mostVotes]} votes</p>
+      </div>
     </div>
   )
 }
 
-export default App
+export default App;
