8608e9030cc3b2b79f15b3c262a440de7f6051b8
Marge <marge.consunji@gmail.com>
2022-08-04 14:58:00 +0800

    Import Course component, clean up code

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,43 +1,4 @@
-const Header = ({ courseName }) => {
-  return <h1>{courseName}</h1>;
-};
-
-const Part = ({ partName, numEx }) => {
-  return (
-    <p>
-      {partName} {numEx}
-    </p>
-  );
-};
-
-const Content = ({ parts }) => {
-  return (
-    <>
-      {parts.map(({ id, name, exercises }) => (
-        <Part key={id} partName={name} numEx={exercises} />
-      ))}
-    </>
-  );
-};
-
-const Total = ({ parts }) => {
-  const total = parts.reduce((acc, { exercises }) => acc + exercises, 0);
-  return <p><b>Total of {total} exercieses</b></p>;
-};
-
-const Course = ({ courses }) => {
-  return (
-    <div>
-      {courses.map(({ id, name, parts }) => (
-        <div key={id}>
-          <Header courseName={name} />
-          <Content parts={parts} />
-          <Total parts={parts} />
-        </div>
-      ))}
-    </div>
-  );
-};
+import Course from './components/Course';
 
 const App = () => {
   const courses = [
