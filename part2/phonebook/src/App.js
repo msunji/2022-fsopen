6dcc77577d3e35b3f98a4a9d98ef1c9116ff7a75
Marge <marge.consunji@gmail.com>
2022-08-04 15:45:06 +0800

    Add functionality to add a new person to phonebook

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,20 +1,37 @@
+import { useState } from 'react';
+
 const App = () => {
   const [persons, setPersons] = useState([{ name: 'Arto Hellas' }]);
   const [newName, setNewName] = useState('');
 
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    const personObj = {
+      name: newName
+    }
+    setPersons(persons.concat(personObj)); // note that concat returns a new array
+    setNewName('');
+  }
+
+  const handleNameChange = (e) => {
+    setNewName(e.target.value);
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={handleFormSubmit}>
         <div>
-          name: <input />
+          name: <input onChange={handleNameChange} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <ul>
+        {persons.map((person) => <li key={person.name}>{person.name}</li>)}
+      </ul>
     </div>
   );
 };
