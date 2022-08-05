f18a9f94fe91ab072af9e198d8b5cb77e33e7391
Marge <marge.consunji@gmail.com>
2022-08-05 16:34:42 +0800

    Show weather data for single country view

--- part2/countries/src/components/View.js
+++ part2/countries/src/components/View.js
@@ -1,7 +1,28 @@
-const View = ({ countryInfo }) => {
-  const { name, flags, languages, capital, area } = countryInfo;
+const WeatherView = ({ capital, weatherData }) => {
+  const { main, wind, weather } = weatherData;
+  const { icon, description } = weather[0];
   return (
     <div>
+      <h2>Weather in {capital}</h2>
+      <p>
+        <b>Temperature:</b> {main.temp} celsius
+      </p>
+      <img
+        src={`http://openweathermap.org/img/wn/${icon}@2x.png`}
+        alt={description}
+      />
+      <p>
+        <b>Wind:</b> {wind.speed} m/s
+      </p>
+    </div>
+  );
+};
+
+const View = ({ countryInfo, showWeather }) => {
+  const { name, flags, languages, capital, area, weatherData } = countryInfo;
+  console.log(weatherData);
+  return (
+    <>
       <div>
         <h1>{name.common}</h1>
         <p>
@@ -14,15 +35,18 @@ const View = ({ countryInfo }) => {
       <div>
         <b>languages</b>
         <ul>
-          {
-            Object.values(languages).map((language) => <li key={language}>{language}</li>)
-          }
+          {Object.values(languages).map((language) => (
+            <li key={language}>{language}</li>
+          ))}
         </ul>
       </div>
       <div>
         <img src={flags.svg} alt={`${name.common} flag`} width="400" />
       </div>
-    </div>
+      {showWeather ? (
+        <WeatherView capital={capital} weatherData={weatherData} />
+      ) : null}
+    </>
   );
 };
 
