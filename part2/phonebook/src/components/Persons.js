5e5f3901e979ed8befa4d6c3f34a8791c6513f57
Marge <marge.consunji@gmail.com>
2022-08-10 14:20:20 +0800

    Add button that deletes entry on click

--- part2/phonebook/src/components/Persons.js
+++ part2/phonebook/src/components/Persons.js
@@ -1,11 +1,11 @@
-const Persons = ({ persons, filterName }) => {
+const Persons = ({ persons, filterName, handleDelete }) => {
   return (
     <ul>
       {persons
         .filter(({ name }) => name.toLowerCase().includes(filterName.toLowerCase()))
-        .map(({ name, number }) => (
+        .map(({ id, name, number }) => (
           <li key={name}>
-            {name} {number}
+            {name} {number} <button onClick={() => handleDelete(id, name)}>delete</button>
           </li>
         ))}
     </ul>
