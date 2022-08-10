29ae757ef36943310ce10e6e0dd43263552141ce
Marge <marge.consunji@gmail.com>
2022-08-10 15:32:10 +0800

    Refactor some functions, fix form inputs
    - Pass state (newName, newNumber) to form inputs
    - Move updating a phonebook entry into its own function

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -11,33 +11,39 @@ const App = () => {
   const [filter, setFilter] = useState('');
 
   useEffect(() => {
-    personService.getEntries().then(allEntries => {
+    personService.getEntries().then((allEntries) => {
       setPersons(allEntries);
-    })
+    });
   }, []);
 
-  const handleFormSubmit = (e) => {
-    const checkNameExists = (name) => {
-      return persons.some(
-        (person) => person.name.toLowerCase() === name.toLowerCase()
+  const entryObj = {
+    name: newName,
+    number: newNumber,
+  };
+
+  const handleUpdate = () => {
+    if (
+      window.confirm(
+        `${newName} already exists. Would you like to replace the old number with a new one?`
+      )
+    ) {
+      const entryToUpdate = persons.find(
+        (person) => person.name.toLowerCase() === newName.toLowerCase()
       );
-    };
-    e.preventDefault();
+      const updatedEntry = { ...entryToUpdate, number: newNumber };
 
-    if (checkNameExists(newName))
-      return alert(`${newName} has already been added to the phonebook.`);
-
-    const entryObj = {
-      name: newName,
-      number: newNumber,
-    };
-
-    personService.addEntry(entryObj).then(newEntry => {
-      setPersons(persons.concat(newEntry));
-      // note that concat returns a new array
-      setNewName('');
-      setNewNumber('');
-    })
+      personService
+        .updateEntry(updatedEntry.id, updatedEntry)
+        .then((updated) => {
+          setPersons(
+            persons.map((person) =>
+              person.id === updatedEntry.id ? updated : person
+            )
+          );
+          setNewName(() => '');
+          setNewNumber(() => '');
+        });
+    }
   };
 
   const handleNameChange = (e) => {
@@ -51,13 +57,40 @@ const App = () => {
   const handleDeleteEntry = (id, name) => {
     if (window.confirm(`Delete ${name}?`)) {
       personService.deleteEntry(id);
-      setPersons(persons.filter(person => person.id !== id));
+      setPersons(persons.filter((person) => person.id !== id));
     }
     return;
-  }
+  };
 
   const handleFilterChange = (e) => {
     setFilter(e.target.value);
+  };
+
+  const handleFormSubmit = (e) => {
+    const nameExists = (name) => {
+      return persons.some(
+        (person) => person.name.toLowerCase() === name.toLowerCase()
+      );
+    };
+
+    const numExists = (num) => {
+      return persons.some((person) => person.number === num);
+    };
+    e.preventDefault();
+
+    if (nameExists(newName) && numExists(newNumber))
+      return alert(
+        `An entry for ${newName} with the same number already exists.`
+      );
+
+    if (nameExists(newName) && !numExists(newNumber)) return handleUpdate();
+
+    return personService.addEntry(entryObj).then((newEntry) => {
+      setPersons(persons.concat(newEntry));
+      // note that concat returns a new array
+      setNewName(() => '');
+      setNewNumber(() => '');
+    });
   };
 
   return (
@@ -67,6 +100,8 @@ const App = () => {
       <div>
         <h2>Add New Entry</h2>
         <PersonInputs
+          name={newName}
+          number={newNumber}
           handleSubmit={handleFormSubmit}
           handleName={handleNameChange}
           handleNumber={handleNumberChange}
@@ -74,7 +109,11 @@ const App = () => {
       </div>
       <div>
         <h2>Numbers</h2>
-        <Persons persons={persons} filterName={filter} handleDelete={handleDeleteEntry} />
+        <Persons
+          persons={persons}
+          filterName={filter}
+          handleDelete={handleDeleteEntry}
+        />
       </div>
     </div>
   );
