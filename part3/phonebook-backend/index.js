a532f11e1cd36addf0cebe2c266c48a4b268d5c1
Marge <marge.consunji@gmail.com>
2022-08-18 14:58:25 +0800

    Configure morgan

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 const PORT = 3001;
 
 app.use(express.json());
+
+morgan.token('body', (req, res) => {
+  return JSON.stringify(req.body); // convert a JS object or value to a JSON string
+})
+
+app.use(morgan(':method :url :status :res[content-length] :response-time ms - :body'));
 
 let persons = [
   {
